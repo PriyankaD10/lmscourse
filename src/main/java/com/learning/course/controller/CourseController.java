6d45b30261bace2c7099112ac1472a020d1f38eb
PriyankaD10 <priyanka.ds10@gmail.com>
2022-11-17 16:34:55 +0530

    Connection str for azuredb updated

--- src/main/java/com/learning/course/controller/CourseController.java
+++ src/main/java/com/learning/course/controller/CourseController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/courses")
 @Slf4j
-@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CourseController {
     @Autowired
     private CourseService courseService;
